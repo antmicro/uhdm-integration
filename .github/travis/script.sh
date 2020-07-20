94a1c9682f518617530768414e8e54d283e066e4
Rafal Kapuscik <rkapuscik@antmicro.com>
2020-07-20 09:20:55 +0200

    First try for cache warmup
    Signed-off-by: Rafal Kapuscik <rkapuscik@antmicro.com>

--- .github/travis/script.sh
+++ .github/travis/script.sh
@@ -11,6 +11,10 @@ case $MODE in
 	make -j $(nproc) surelog/regression
 	;;
 
+    cache-warmup)
+	make -j $(nproc) ENABLE_READLINE=0 PRETTY=0 surelog uhdm/verilator/build yosys/yosys
+	;;
+
     test)
 	make -j $(nproc) ENABLE_READLINE=0 PRETTY=0 TEST=$TEST_CASE $TARGET
 	;;
