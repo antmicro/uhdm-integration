b6fec633087622b48b8283c513df5f2c7296882c
Kamil Rakoczy <krakoczy@antmicro.com>
2020-11-17 16:58:45 +0100

    Add diff_verilog.py script
    Signed-off-by: Kamil Rakoczy <krakoczy@antmicro.com>

---
+++ diff_verilog.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+import re
+import os
+import subprocess
+
+def parse_file(file_path):
+    module_dict = {}
+    current_module_name = ""
+    with open(file_path, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            if line.startswith("module"):
+                r = re.search('module(.*)\(', line)
+                module_name = r.group(1)
+                if "\\$paramod" in module_name:
+                    current_module_name = module_name.replace("\\$paramod", "")
+                else:
+                    current_module_name = module_name
+                begin = current_module_name.find("\\")
+                if begin != -1:
+                    end = current_module_name.find("\\", begin + 1, len(current_module_name))
+                    current_module_name = current_module_name[begin:end]
+                if current_module_name == "":
+                    current_module_name = module_name
+                current_module_name = current_module_name.replace(' ', '')
+                current_module_name = current_module_name.replace('\\', '')
+                #print(f"{current_module_name} -> {module_name}")
+
+
+            if current_module_name != "":
+                if current_module_name in module_dict:
+                    module_dict[current_module_name].append(line)
+                else:
+                    module_dict[current_module_name] = [line]
+
+            line = f.readline()
+
+    return module_dict
+
+surelog = parse_file('build/top.v')
+sv = parse_file('build-sv/top.v')
+
+os.makedirs("compare", exist_ok=True)
+
+for key in sorted(surelog.keys()):
+    with open(f'compare/surelog_{key}.v', 'w') as f:
+        f.writelines(surelog[key])
+
+for key in sorted(sv.keys()):
+    with open(f'compare/sv_{key}.v', 'w') as f:
+        f.writelines(sv[key])
+
+for key1, key2 in zip(sorted(surelog.keys()), sorted(sv.keys())):
+    if (key1 != key2):
+        print(f"Difference in keys!: {key1}   ->   {key2}")
+    subprocess.run(["diff", "-y", "--color=always", "--suppress-common-lines", f"compare/surelog_{key1}.v", f"compare/sv_{key2}.v"])
+
