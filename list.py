19f18f8e1f7beba1a2a06963d13c8118522c1e40
Kamil Rakoczy <krakoczy@antmicro.com>
2020-11-30 12:31:21 +0100

    Update list.py
    Signed-off-by: Kamil Rakoczy <krakoczy@antmicro.com>

--- list.py
+++ list.py
@@ -10,7 +10,11 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--directory', '-d', type=str,
                     help='generate list from the specifiec directory')
 
+parser.add_argument('--skip-elements', '-s',
+                    nargs='*', default=[],
+                    help='list of elements to skip')
+
 args = parser.parse_args()
 
 # skip ibex test as it is handled separately
-print(json.dumps([f"tests/{node}" for node in os.listdir(args.directory) if node != "ibex"]))
+print(json.dumps([f"tests/{node}" for node in os.listdir(args.directory) if node not in args.skip_elements]))
